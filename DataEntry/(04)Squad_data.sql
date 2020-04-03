6a825d205d4d9c447102b8f7dc682594d4e1f320
Kartikeya Shorya <kartikeyashorya@gmail.com>
2020-04-03 12:45:00 +0530

    Hope it is final

--- DataEntry/(04)Squad_data.sql
+++ DataEntry/(04)Squad_data.sql
@@ -13,7 +13,6 @@ INSERT INTO Squads (squadNumber,Captain,yearNo,totalCapacity) VALUES('82nd Airbo
 INSERT INTO Squads (squadNumber,Captain,yearNo,totalCapacity) VALUES('23rd Infantry Division',15346,2005,55);
 INSERT INTO Squads (squadNumber,Captain,yearNo,totalCapacity) VALUES('50th Independent Parachute Brigade',12345,2013,60);
 INSERT INTO Squads (squadNumber,Captain,yearNo,totalCapacity) VALUES('39th Infantry Dvision',15237,2013,40);
-INSERT INTO Squads (squadNumber,Captain,yearNo,totalCapacity) VALUES('50th Independent Parachute Brigade',48563,2005,50);
 INSERT INTO Squads (squadNumber,Captain,yearNo,totalCapacity) VALUES('29st Infantry Division',13045,2014,45);
 INSERT INTO Squads (squadNumber,Captain,yearNo,totalCapacity) VALUES('21st Infantry Division',11045,2014,55);
 INSERT INTO Squads (squadNumber,Captain,yearNo,totalCapacity) VALUES('31st Infantry Division',22045,2014,60);
@@ -21,17 +20,5 @@ INSERT INTO Squads (squadNumber,Captain,yearNo,totalCapacity) VALUES('11st Infan
 INSERT INTO Squads (squadNumber,Captain,yearNo,totalCapacity) VALUES('61st Infantry Division',21005,2013,45);
 INSERT INTO Squads (squadNumber,Captain,yearNo,totalCapacity) VALUES('81st Infantry Division',21045,2003,50);
 INSERT INTO Squads (squadNumber,Captain,yearNo,totalCapacity) VALUES('71st Infantry Division',11045,2016,65,);
-INSERT INTO Squads (squadNumber,Captain,yearNo,totalCapacity) VALUES('7th Infantry Division',11305,2013,35);
-INSERT INTO Squads (squadNumber,Captain,yearNo,totalCapacity) VALUES('34th Mountain Division',12005,2017,45);
-INSERT INTO Squads (squadNumber,Captain,yearNo,totalCapacity) VALUES('71st Infantry Division',10045,2010,60);
-INSERT INTO Squads (squadNumber,Captain,yearNo,totalCapacity) VALUES('50th Independent Parachute Brigade',10345,2016,45);
-INSERT INTO Squads (squadNumber,Captain,yearNo,totalCapacity) VALUES('7th Infantry Division',12045,2015,50);
 INSERT INTO Squads (squadNumber,Captain,yearNo,totalCapacity) VALUES('4th Armoured Division',12115,2012,55);
-INSERT INTO Squads (squadNumber,Captain,yearNo,totalCapacity) VALUES('7th Infantry Division',11145,2015,60);
-INSERT INTO Squads (squadNumber,Captain,yearNo,totalCapacity) VALUES('50th Independent Parachute Brigade',14345,2008,65);
-INSERT INTO Squads (squadNumber,Captain,yearNo,totalCapacity) VALUES('34th Mountain Division',12105,2017,60);
-INSERT INTO Squads (squadNumber,Captain,yearNo,totalCapacity) VALUES('4th Armoured Division',12300,2010,65);
-INSERT INTO Squads (squadNumber,Captain,yearNo,totalCapacity) VALUES('34th Mountain Division',12051,2015,70);
 INSERT INTO Squads (squadNumber,Captain,yearNo,totalCapacity) VALUES('39th Infantry Division',10305,2016,55);
-INSERT INTO Squads (squadNumber,Captain,yearNo,totalCapacity) VALUES('34th Mountain Division',12245,2014,55);
-
