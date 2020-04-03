47aa93376d9c7f7acb8e833b638cdfb729a389de
Kartikeya Shorya <kartikeyashorya@gmail.com>
2020-04-03 11:45:53 +0530

    Added data for rewards table

---
+++ DataEntry/(14)Reward_data.sql
@@ -0,0 +1,16 @@
+--data entry for rewards
+INSERT INTO REWARD(ID, MedalName, Year) VALUES (44668,'Samanya Seva Medal', 2005);
+INSERT INTO REWARD(ID, MedalName, Year) VALUES (17694, 'Vir Chakra', 2005);
+INSERT INTO REWARD(ID, MedalName, Year) VALUES (14256, 'Samanya Seva Medal', 2002);
+INSERT INTO REWARD(ID, MedalName, Year) VALUES (11053, 'Uttam Yuda Seva Medal', 1999);
+INSERT INTO REWARD(ID, MedalName, Year) VALUES (22513, 'MacGregor Medal', 2010);
+INSERT INTO REWARD(ID, MedalName, Year) VALUES (31931, 'Sena Medal', 1980);
+INSERT INTO REWARD(ID, MedalName, Year) VALUES (19339, 'Vir Chakra', 1995);
+INSERT INTO REWARD(ID, MedalName, Year) VALUES (13423, 'Samanya Seva Medal', 1997);
+INSERT INTO REWARD(ID, MedalName, Year) VALUES (49159, 'Wound Medal(India)', 1997);
+INSERT INTO REWARD(ID, MedalName, Year) VALUES (21474, 'Vishist Seva Medal', 2002);
+INSERT INTO REWARD(ID, MedalName, Year) VALUES (14298, 'Saichen Glacier Medal', 2000);
+INSERT INTO REWARD(ID, MedalName, Year) VALUES (10923, 'Saichen Glacier Medal', 2000);
+INSERT INTO REWARD(ID, MedalName, Year) VALUES (12398, 'Samanya Seva Medal', 2000);
+INSERT INTO REWARD(ID, MedalName, Year) VALUES (10305, 'India Service medal', 2005);
+INSERT INTO REWARD(ID, MedalName, Year) VALUES (12115, 'Wound Medal(India)', 1990);
